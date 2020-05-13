6eaaf845379342751106430ca9edb7d18894aee0
Alex Doss <alex.doss42@gmail.com>
2020-05-12 21:06:21 -0600

    Adds campgrounds list

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-  res.render("landing")
+  res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+  var campgrounds = [
+    {name: "Uranium Valley", image: "https://www.ohsosavvymom.com/wp-content/uploads/2017/05/Behind-the-Reef-Road-Temple-Mountain-Camping-on-BLM-land-at-Goblin-Valley.jpg"},
+    {name: "Goblin's Lair", image: "https://sp-images.summitpost.org/835994.jpg?auto=format&fit=max&h=800&ixlib=php-2.1.1&q=35&s=f8c0015266577409e89469e7d73f8c4a"},
+    {name: "Iceland's Secret", image: "https://www.freshoffthegrid.com/wp-content/uploads/Camping-in-Iceland.jpg"}
+  ]
 });
 
 app.listen(3001, () => console.log(`It's over Anakin. I have the high ground`));
