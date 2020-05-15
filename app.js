f2cb9d5a8ba1e2e06e7a495aeceb02a57a34d92c
Alex Doss <alex.doss42@gmail.com>
2020-05-15 16:32:42 -0600

    Adds logic to add campgrounds to list

--- app.js
+++ app.js
@@ -5,27 +5,33 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+  {name: "Uranium Valley", image: "https://www.ohsosavvymom.com/wp-content/uploads/2017/05/Behind-the-Reef-Road-Temple-Mountain-Camping-on-BLM-land-at-Goblin-Valley.jpg"},
+  {name: "Goblin's Lair", image: "https://sp-images.summitpost.org/835994.jpg?auto=format&fit=max&h=800&ixlib=php-2.1.1&q=35&s=f8c0015266577409e89469e7d73f8c4a"},
+  {name: "Iceland's Secret", image: "https://www.freshoffthegrid.com/wp-content/uploads/Camping-in-Iceland.jpg"}
+]
+
 app.get("/", function(req, res){
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-  var campgrounds = [
-    {name: "Uranium Valley", image: "https://www.ohsosavvymom.com/wp-content/uploads/2017/05/Behind-the-Reef-Road-Temple-Mountain-Camping-on-BLM-land-at-Goblin-Valley.jpg"},
-    {name: "Goblin's Lair", image: "https://sp-images.summitpost.org/835994.jpg?auto=format&fit=max&h=800&ixlib=php-2.1.1&q=35&s=f8c0015266577409e89469e7d73f8c4a"},
-    {name: "Iceland's Secret", image: "https://www.freshoffthegrid.com/wp-content/uploads/Camping-in-Iceland.jpg"}
-  ]
-
-  app.post("/campgrounds", function(req, res){
-    res.send('You hit the post route')
-  });
-
   res.render("campgrounds", { campgrounds: campgrounds });
 });
 
+app.post("/campgrounds", function(req, res){
+  res.send('You hit the post route')
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+  
+
 app.get("/campground/new", function(req, res){
   res.render("new.ejs");
-})
+});
 
 app.listen(3001, () => console.log(`It's over Anakin. I have the high ground`));
 
