c2254bf41a98c52791c67ab478d886692bfb80f3
Alex Doss <alex.doss42@gmail.com>
2020-05-17 19:22:45 -0600

    Adds mongoose find and create

--- app.js
+++ app.js
@@ -3,21 +3,48 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-  {name: "Uranium Valley", image: "https://www.ohsosavvymom.com/wp-content/uploads/2017/05/Behind-the-Reef-Road-Temple-Mountain-Camping-on-BLM-land-at-Goblin-Valley.jpg"},
-  {name: "Goblin's Lair", image: "https://sp-images.summitpost.org/835994.jpg?auto=format&fit=max&h=800&ixlib=php-2.1.1&q=35&s=f8c0015266577409e89469e7d73f8c4a"},
-  {name: "Iceland's Secret", image: "https://www.freshoffthegrid.com/wp-content/uploads/Camping-in-Iceland.jpg"}
-]
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+Campground.create(
+  {
+    name: "Salmon Creek",
+    image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
+  }, function(err, campground){
+    if(err){
+      console.log(err);
+    } else {
+      console.log("NEWLY CREATED CAMPGROUND: ");
+      console.log(campground);
+    }
+  });
+
+// var campgrounds = [
+//   {name: "Uranium Valley", image: "https://www.ohsosavvymom.com/wp-content/uploads/2017/05/Behind-the-Reef-Road-Temple-Mountain-Camping-on-BLM-land-at-Goblin-Valley.jpg"},
+//   {name: "Goblin's Lair", image: "https://sp-images.summitpost.org/835994.jpg?auto=format&fit=max&h=800&ixlib=php-2.1.1&q=35&s=f8c0015266577409e89469e7d73f8c4a"},
+//   {name: "Iceland's Secret", image: "https://www.freshoffthegrid.com/wp-content/uploads/Camping-in-Iceland.jpg"}
+// ]
 
 app.get("/", function(req, res){
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-  res.render("campgrounds", { campgrounds: campgrounds });
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log(err)
+    } else {
+      res.render("campgrounds", {campgrounds: allCampgrounds});
+    }
+  })
 });
 
 app.post("/campgrounds", function(req, res){
