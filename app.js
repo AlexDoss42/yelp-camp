ed236ce3082ee6fcb75576c1523992cea887df96
Alex Doss <alex.doss42@gmail.com>
2020-05-25 19:23:54 -0600

    adds comment logic

--- app.js
+++ app.js
@@ -71,7 +71,32 @@ app.get("/campgrounds/:id", function(req, res){
 // Comments routes
 
 app.get("/campgrounds/:id/comments/new", function(req, res){
-  res.render("comments/new");
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+      if(err){
+        console.log(err);
+        res.redirect("/campgounds");
+      } else {
+        Comment.create(req.body.comment, function(err, comment){
+          if(err){
+            console.log(err);
+          } else {
+            campground.comments.push(comment);
+            campground.save();
+            res.redirect("/campgrounds/" + campground._id)
+          }
+        })
+      }
+    });
 });
 
 app.listen(3001, () => console.log(`It's over Anakin. I have the high ground`));
