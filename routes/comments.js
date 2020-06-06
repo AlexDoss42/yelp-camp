c73b8c38bf380ecf5305bd2b8ba4a5d3dabf8e07
Alex Doss <alex.doss42@gmail.com>
2020-06-05 19:13:21 -0600

    Adds success messages for comments

--- routes/comments.js
+++ routes/comments.js
@@ -21,6 +21,7 @@ router.post("/campgrounds/:id/comments", middleware.isLoggedIn, function(req, re
       } else {
         Comment.create(req.body.comment, function(err, comment){
           if(err){
+            req.flash("error", "Something went wrong");
             console.log(err);
           } else {
             comment.author.id = req.user._id;
@@ -28,6 +29,7 @@ router.post("/campgrounds/:id/comments", middleware.isLoggedIn, function(req, re
             comment.save();
             campground.comments.push(comment);
             campground.save();
+            req.flash("success", "Successfully added comment");
             res.redirect("/campgrounds/" + campground._id)
           }
         })
@@ -40,6 +42,7 @@ router.get("/campgrounds/:id/comments/:comment_id/edit", middleware.checkComment
     if(err){
       res.redirect("back");
     } else {
+      req.flash("success", "Successfully editted comment");
       res.render("/comments/edit", {campground_id: req.params.id, comment: foundComment})
     };
   });
@@ -60,6 +63,7 @@ router.delete("/campgrounds/:id/comments/:comment_id", middleware.checkCommentOw
     if(err){
       res.redirect("back");
     } else {
+      req.flash("success", "Comment Deleted");
       res.redirect("/campgrounds" + req.params.id);
     }
   });
